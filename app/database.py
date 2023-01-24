edfeebea906e5d4823edf47b8722c75f35a75d39
Alireza Abedin <aliab3dev@gmail.com>
2023-01-23 16:53:34 -0800

    SD-10 Update db setup file to add missing semi-colon in db url configuration

--- app/database.py
+++ app/database.py
@@ -5,8 +5,8 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}"
-    f"{settings.database_port}/{settings.database_name}"
+    f"postgresql://{settings.database_username}:{settings.database_password}@"
+    f"{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 )
 
 engine = create_engine(url=SQLALCHEMY_DATABASE_URL)
